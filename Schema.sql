13dc45f658835f19d8bd6b6c8e5fe17caa4764f8
Bruno <bfcarvalho2002@gmail.com>
2021-11-02 01:52:31 -0300

    Adicionando comando de insert para rodar os testes unitarios

--- Schema.sql
+++ Schema.sql
@@ -8,3 +8,10 @@ CREATE TABLE
 		cons_med_cidade DOUBLE, 
 		cons_med_rodov DOUBLE
 	);
+	
+INSERT INTO 
+	VEICULO(
+		NOME, MARCA, MODELO, DATA_FABRIC, CONS_MED_CIDADE, CONS_MED_RODOV
+	) 
+VALUES 
+	('Nome X', 'Hyundai', 'I30', '2021-11-02', 10, 12);
