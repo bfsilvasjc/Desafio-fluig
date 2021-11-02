ffb85876d458140cb488356eef5b8e0d3b56971d
Bruno <bfcarvalho2002@gmail.com>
2021-11-02 02:32:56 -0300

    Adicionando arquivo de configurações do swagger

---
+++ api-desafio/src/main/java/com/teste/bruno/apidesafio/config/SwaggerUiWebMvcConfigurer.java
@@ -0,0 +1,11 @@
+package com.teste.bruno.apidesafio.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class SwaggerUiWebMvcConfigurer implements WebMvcConfigurer {
+    private final String baseUrl;
+
+    public SwaggerUiWebMvcConfigurer(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+}
